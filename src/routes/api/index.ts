e9584d2c90a534bcd9b77ba3d2395dd9b0dfca69
Soel30 <soeltan216@gmail.com>
2023-01-12 12:28:51 +0700

    delete provincies routes

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
-import provinces from "@components/Provinces/routes";
 import users from "@components/users/routes";
 import auth from "@components/auth/routes";
 const router: Router = Router();
 
-router.use("/provinces", provinces);
 router.use("/users", users);
 router.use("/", auth);
 
