3bb0044181695938e9938ef11b7ae71f0eec4362
Soel30 <soeltan216@gmail.com>
2023-01-12 12:28:16 +0700

    delete provincies resource

--- src/components/Provinces/controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export default class ProvinceController {
-  public async index(req: Request, res: Response) {
-    res.json({ message: "Hello World!" });
-  }
-}
--- src/components/Provinces/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import ProvinceController from "./controller";
-const router: Router = Router();
-
-const controller: ProvinceController = new ProvinceController();
-
-router.get("/", controller.index);
-
-export default router;
